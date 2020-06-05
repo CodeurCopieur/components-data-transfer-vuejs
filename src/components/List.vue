92d6352b7e452ab2ca1ecc834867a3f450153894
codwerk <louis_widdy@live.fr>
2020-06-05 22:03:41 +0200

    create component List

---
+++ src/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Component List</h1>
+</template>
+
+<script>
+
+
+export default {
+  name: 'List',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
